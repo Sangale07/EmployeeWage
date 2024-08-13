5b70eed253ae876278cc21e0255de27c93cca193
rushi <rushikeshsangale57@gmail.com>
2024-08-13 14:30:20 +0530

    DailyWage Function is Created

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,21 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+    private int wagePerHr=20;
+    private int FullDayHr=8;
+    private int totalWage=0;
+
+    public void dailywage(boolean add){
+
+        if(add==true){
+
+            totalWage=totalWage+(wagePerHr *FullDayHr);
+        }else{
+
+            totalWage=totalWage+0;
+        }
+        System.out.println("TotalWage= "+totalWage);
+    }
     public static  void isPresent(){
         Random random = new Random();
         // Generate a random number (0 or 1)
