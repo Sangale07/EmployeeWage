a55b99878000c373e1dad036d12accefa4c0cd49
rushi <rushikeshsangale57@gmail.com>
2024-08-13 14:25:45 +0530

    Added ispresent function

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+    }
+    public static  void isPresent(){
+        Random random = new Random();
+        // Generate a random number (0 or 1)
+        int attendance = random.nextInt(2);
+
+        // Check if the employee is present or absent
+        if (attendance == 0) {
+            System.out.println("Employee is absent.");
+        } else {
+            System.out.println("Employee is present.");
+        }
     }
 }
